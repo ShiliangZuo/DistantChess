02084b89147f1becfc83eac96916eee66f8ad215
ShiliangZuo <zuo.shiliang@gmail.com>
2017-10-31 22:10:24 +0800

    start on displaying depth

---
+++ Assets/Model Generation/DepthManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+public class DepthManager : MonoBehaviour {
+
+    private KinectSensor sensor;
+    private DepthFrameReader depthReader;
+    private ushort[] depthData;
+
+    public ushort[] getDepthData() {
+        return depthData;
+    }
+
+	// Use this for initialization
+	void Start () {
+        sensor = KinectSensor.GetDefault();
+
+        if (sensor != null) {
+            depthReader = sensor.DepthFrameSource.OpenReader();
+            depthData = new ushort[sensor.DepthFrameSource.FrameDescription.LengthInPixels];
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (depthReader != null) {
+
+        }
+	}
+}
